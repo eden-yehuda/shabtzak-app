938f76d769bab8b27126742e25de23cd9aec2b2a
Shabtzak Dev <shabtzak@local.dev>
2026-04-28 20:25:59 +0300

    fix: force light color-scheme for cross-browser mobile compatibility

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="he" dir="rtl">
       <Head>
+        <meta name="color-scheme" content="light" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
